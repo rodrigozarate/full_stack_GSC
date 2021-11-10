4899e38fcec45f356f5d227eefeefc257f9f4fff
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-11-09 23:49:58 -0500

    [CREATE]

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<title>Columbus Backery Wholesale</title>
+        
+	</head>
+	<body>
+		<div class="main">
+        <div class="navbar sidebar"></div>
+        <div class="content">
+            <div class="topbar">
+                <div class="logo"><img src="logo.png"/></div>
+                <div class="userprofile"><img src="user.png"/></div>
+            </div>
+            <div class="panel">
+                <div class="col1">
+                    <div class="listrow">
+                        <div class="itemheader"></div>
+                        <div class="item">
+                            <div class="cant">1</div>
+                            <div class="plusbutton">+</div>
+                            <div class="minusbutton">-</div>
+                            <div class="description">COCA-COLA</div>
+                            <div class="tax">3.5</div>
+                            <div class="total">3.5</div>
+                            <div class="destroy">X</div>
+                        </div>
+                        <div class="item">
+                            <div class="cant">1</div>
+                            <div class="plusbutton">+</div>
+                            <div class="minusbutton">-</div>
+                            <div class="description">MANZANA POSTOBON</div>
+                            <div class="tax">3.5</div>
+                            <div class="total">3.5</div>
+                            <div class="destroy">X</div>
+                        </div>
+                        <div class="item">
+                            <div class="cant">1</div>
+                            <div class="plusbutton">+</div>
+                            <div class="minusbutton">-</div>
+                            <div class="description">NARANJA POSTOBON</div>
+                            <div class="tax">3.5</div>
+                            <div class="total">3.5</div>
+                            <div class="destroy">X</div>
+                        </div>
+                    </div>
+                    <div class="discountrow"></div>
+                    <div class="cuponrow"></div>
+                </div>
+                <div class="col2">
+                    <div class="tabsrow"></div>
+                    <div class="gridrow"></div>
+                    <div class="paymentrow"></div>
+                </div>
+            </div>
+        </div>
+        </div>
+	</body>
+</html>
